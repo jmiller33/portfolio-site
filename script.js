c2c9b54fd69476d163c79ab00796c87f177f1e49
Jazzlyn <millerjazzlyna@gmail.com>
2022-08-21 02:45:14 -0500

    Reconnected contact buttons to links, after having broken them accidentally in the last edit

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ webDesignButton.addEventListener('click', () => {
 illustrationButton.addEventListener('click', () => {
     window.location.href = "illustrations.html";
 });
+
+const instagram = document.querySelector("#ig");
+const linkedIn = document.querySelector("#li");
+const email = document.querySelector("#email");
+
+instagram.addEventListener('click', () => {
+    window.location.href = "https://www.instagram.com/jazzlyn.miller.designs";
+});
+
+linkedIn.addEventListener('click', () => {
+    window.location.href = "https://www.linkedin.com";
+});
+
+email.addEventListener('click', () => {
+    window.location.href = "mailto: millerjazzlyna@gmail.com";
+});
